7f306d403b0caede71157fc955a54a342b5d1e1a
chigvintseva <al.chigvintseva@gmail.com>
2025-09-11 19:15:49 +0200

    client is able to get args, send signals and pauses. not sure whether signals are send correctly, no idea how to acknowledge apart from pause - maybe condition to get exactly 1, not sure about efficiency and aknowledgement of the null terminator - i need to make sure it is also finished and considered the end of this client, thus, signals from another client can be starting to be received.

--- client.c
+++ client.c
@@ -6,20 +6,50 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 13:55:33 by achigvin          #+#    #+#             */
-/*   Updated: 2025/09/11 15:09:42 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/09/11 19:12:37 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+void	send_byte(char current_byte, pid_t server_pid)
+{
+	int	bit;
+
+	bit = 7;
+	while (bit >= 0)
+	{
+		if (((current_byte >> bit) & 1) == 1)
+		{
+			kill(server_pid, SIG_BIT_ONE);
+			pause();		
+		}
+
+		else
+		{
+			kill(server_pid, SIG_BIT_ZERO);
+			pause();
+		}
+		bit--;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	pid_t	server_pid;
 	char	*message;
+	long	current_byte;
 
 	if (argc != 3)
 		return (args_error_msg(), 0);
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
+	current_byte = 0;
+	while (message[current_byte] != '\0')
+	{
+		send_byte(message[current_byte], server_pid);
+		current_byte++;
+	}
+	send_byte(message[current_byte], server_pid);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: achigvin <achigvin@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/10 13:55:46 by achigvin          #+#    #+#             */
-/*   Updated: 2025/09/10 14:15:01 by achigvin         ###   ########.fr       */
+/*   Updated: 2025/09/11 16:59:59 by achigvin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,15 @@
 int	main(void)
 {
 	pid_t	server_pid;
+	char	byte;
+	int		bits_count;
 	
 	server_pid = getpid();
 	ft_printf("Server's PID: %d\n", (int)server_pid);
-	
+	while (1)
+	{
+		
+	}
 
 	return (0);
 }
