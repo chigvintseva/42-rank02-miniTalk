1fef3bf7daef718ea21727616fc484b56c417191
alena <141341998+chigvintseva@users.noreply.github.com>
2025-09-16 18:59:14 +0200

    client added acknoledgement, fixed signals declaration

--- client.c
+++ client.c
@@ -12,6 +12,11 @@
 
 #include "minitalk.h"
 
+void	ack_from_server(int signal)
+{
+	(void)signal;
+}
+
 void	send_byte(char current_byte, pid_t server_pid)
 {
 	int	bit;
@@ -21,15 +26,13 @@ void	send_byte(char current_byte, pid_t server_pid)
 	{
 		if (((current_byte >> bit) & 1) == 1)
 		{
-			kill(server_pid, SIG_BIT_ONE); 
+			kill(server_pid, SIGUSR1); 
 			pause();
-			// ?? check whetehr the correct signal was received - or server crushed	
 		}
 		else
 		{
-			kill(server_pid, SIG_BIT_ZERO);
+			kill(server_pid, SIGUSR2);
 			pause();
-					// ?? check whetehr the correct signal was received - or server crushed
 		}
 		bit--;
 	}
@@ -40,12 +43,17 @@ int	main(int argc, char **argv)
 	pid_t	server_pid;
 	char	*message;
 	long	current_byte;
+	struct	sigaction sa;
 
 	if (argc != 3)
-		return (args_error_msg(), 0);
+		return (args_error_msg(), 1);
 	server_pid = ft_atoi(argv[1]);
 	message = argv[2];
 	current_byte = 0;
+	sa.sa_handler = ack_from_server;
+	sa.sa_flags = 0;
+	sigemptyset(&sa.sa_mask);
+	sigaction(SIGUSR1, &sa, NULL);
 	while (message[current_byte] != '\0')
 	{
 		send_byte(message[current_byte], server_pid);
